d68ed16c4a228aff0ad3a7f1fa370babbbad16d3
TsolovikosT <teotsolos@hotmail.gr>
2019-03-19 23:13:06 +0200

    Home Page Done
    Home:
    +All content
    +Responsive

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -1,6 +1,41 @@
 // JavaScript Document
-$(document).ready(function () {
-    $("nav").find("li").on("click", "a", function () {
-        $('.navbar-collapse.in').collapse('hide');
-    });
+
+$(document).ready(function () 
+{
+	//nav toggler 
+    $("nav").find("li").on("click", "a", function () 
+		{
+			$('.navbar-collapse.in').collapse('hide');
+		});
+	//Go to top btn
+	$("#go-top-btn").on('click',function()
+		{
+			goTop(); 
+		});
+	//post-carousel
+		$('#carousel-post').carousel({interval: 4000})
+		$('.card .carousel-item').each(function(){
+			var next = $(this).next();
+			if (!next.length) {
+			next = $(this).siblings(':first');
+			}
+			next.children(':first-child').clone().appendTo($(this));
+			for (var i=0;i<2;i++) {
+			next=next.next();
+			if (!next.length) {
+				next = $(this).siblings(':first');
+			}
+
+			next.children(':first-child').clone().appendTo($(this));
+			}});
+
 });
+
+
+
+function goTop() 
+{
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
