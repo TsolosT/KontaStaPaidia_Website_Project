e08c7590bcb24dbe529467610a29292e139bceae
TsolovikosT <teotsolos@hotmail.gr>
2019-03-25 19:43:47 +0200

    Some final Design changes before Testing changes

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -67,6 +67,7 @@ function initMap() {
 //Up to top function
 function goTop() 
 {
+	
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
